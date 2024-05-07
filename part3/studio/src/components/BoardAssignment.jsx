0ea35c6e4505554d9daea2643a595fb45b76ce8a
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-07 00:02:48 -0500

    part 3 studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+   const boards = [
+      { label: '--', value: 'no boards yet!' },
+      { label: 'Desserts', value: 'Desserts' },
+      { label: 'Filipino', value: 'Filipino' },
+      { label: 'Baked Tasties', value: 'Baked Tasties' }
+   ]
+   const [boardName, setName] = useState('no boards yet!')
 
    const handleChange = (event) => {
+      setName(event.target.value)
    }
 
    return (
-      <div style={{paddingTop: "50px"}}>
-      <label>Save to Board: </label>
-      <select value={boardName} onChange={handleChange}>
-      </select>
+      <div style={{ paddingTop: "50px" }}>
+         <label>Save to Board: </label>
+         <select value={boardName} onChange={handleChange}>
+            {boards.map((board, idx) => <option key={idx} value={board.value}>
+               {board.label}
+            </option>)}
+         </select>
 
-      <p>Saved to {boardName}!</p>
+         <p>Saved to {boardName}!</p>
       </div>
-   );
+   )
 }
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,65 +1,74 @@
-import { useState } from 'react';
-
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   const authorLink = 'https://panlasangpinoy.com/about-2/'
+   const authorPhoto = 'https://panlasangpinoy.com/wp-content/uploads/2020/03/Vanjo-Merano-Panlasang-Pinoy.jpg'
+   const authorName = 'Vanjo Merano'
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto}
+            alt="Vanjo Merano"
+            style={{ objectFit: "contain", borderRadius: "50%" }}
+         />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}></a>
          </div>
       </div>
-   );
+   )
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
+   const ingredients = [
+      '2 lbs pork belly',
+      '2 tablespoons garlic',
+      '5 dried bay leaves',
+      '4 tablespoons vinegar',
+      '1/2 cup soy sauce',
+      '1 tablespoon peppercorn',
+      '2 cups water',
+      'Salt to taste'
+   ]
+   return (
       <div>
-         <h3>Recipe Ingredients</h3>
+         <h3>Recipe Ingredients:</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredients.map((ingredient, idx) => <li key={idx}>{ingredient}</li>)}
          </ul>
       </div>
-   );
+   )
 }
 
 const RecipeDescription = () => {
    return (
-      <div> 
+      <div>
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Filipino Pork Adobo</h1>
+            <p>This is a recipe post for Filipino Pork Adobo. It is a dish composed of pork slices cooked in soy sauce, vinegar, and garlic. There are version wherein onions are also added. Adobo is a popular dish in the Philippines, along with Sinigang.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
             <RecipeAuthor />
          </div>
       </div>
-   );
+   )
 }
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
-   );
+      <img src='https://panlasangpinoy.com/wp-content/uploads/2009/08/Pork-Adobo-.jpg'
+         alt='pork adobo'
+         className="imageUpdates"
+      />
+   )
 }
 
-export default function RecipeDisplay () {
-   return(
+export default function RecipeDisplay() {
+   return (
       <div className="recipePhotoBlock">
          <RecipePhoto />
          <div>
             <RecipeDescription />
          </div>
       </div>
-   );
+   )
 }
